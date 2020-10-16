8465c32473db0a3ccc03812da6f98c14a9a49ef1
rvcl3n <d.rudenko.1996@gmail.com>
2020-10-16 19:08:47 +0300

    Added a closures example

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Hello World!');
+//Closures
+//Closure - it`s the combination of a function and the lexical environment within which that function was declared.
+//Example from https://developer.mozilla.org/
+
+function makeAdder(x) {
+    console.log('makeAdder x '+x);
+  return function(y) {
+    console.log('func x '+x+' y '+y);
+    return x + y;
+  };
+}
+
+var add5 = makeAdder(5);
+var add10 = makeAdder(10);
+
+console.log(add5(2));  // 7
+console.log(add10(2)); // 12
