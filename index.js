9da7f6b1a2247e231f1f0dd3aa5049296b5bb4f2
rvcl3n <d.rudenko.1996@gmail.com>
2020-10-16 19:17:29 +0300

    Added one more closure example

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Closures
 //Closure - it`s the combination of a function and the lexical environment within which that function was declared.
-//Example from https://developer.mozilla.org/
+//Example from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures
 
 function makeAdder(x) {
     console.log('makeAdder x '+x);
@@ -15,3 +15,14 @@ var add10 = makeAdder(10);
 
 console.log(add5(2));  // 7
 console.log(add10(2)); // 12
+
+//Example from https://www.w3schools.com/js/js_function_closures.asp
+
+var add = (function () {
+    var counter = 0;
+    return function () {counter += 1; return counter;}
+  })();
+
+  console.log(add());
+  console.log(add());
+  console.log(add());
