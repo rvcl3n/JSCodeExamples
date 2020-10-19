e9ea44226c0ea8a1a35c9accc112cf508fc3d984
rvcl3n <d.rudenko.1996@gmail.com>
2020-10-19 22:28:17 +0300

    Added a new example

--- variables.js
+++ variables.js
@@ -1,5 +1,6 @@
-//Example from https://www.toptal.com/javascript/interview-questions
+//Examples from https://www.toptal.com/javascript/interview-questions
 
+//Example 1
 (function(){
     var a = b = 3;
   })();
@@ -13,3 +14,18 @@
 
 console.log(0.1 + 0.2);
 console.log(0.1 + 0.2 == 0.3);
+
+//Example 2
+var myObject = {
+  foo: "bar",
+  func: function() {
+      var self = this;
+      console.log("outer func:  this.foo = " + this.foo);
+      console.log("outer func:  self.foo = " + self.foo);
+      (function() {
+          console.log("inner func:  this.foo = " + this.foo);
+          console.log("inner func:  self.foo = " + self.foo);
+      }());
+  }
+};
+myObject.func();
